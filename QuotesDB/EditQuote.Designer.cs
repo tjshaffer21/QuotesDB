09749c0f34731fe433fea46bfacd2221d01cd38c
Thomas Shaffer <tjshaffer21@gmail.com>
2011-02-06 04:28:58 -0600

    Included code to create tables on new database. Modified database layout. Started EditQuote form.

---
+++ QuotesDB/EditQuote.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace QuotesDB
+{
+    partial class EditQuote
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.authorText = new System.Windows.Forms.TextBox();
+            this.locationText = new System.Windows.Forms.TextBox();
+            this.tagText = new System.Windows.Forms.TextBox();
+            this.quoteText = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // authorText
+            // 
+            this.authorText.Location = new System.Drawing.Point(75, 22);
+            this.authorText.Name = "authorText";
+            this.authorText.Size = new System.Drawing.Size(406, 20);
+            this.authorText.TabIndex = 0;
+            // 
+            // locationText
+            // 
+            this.locationText.Location = new System.Drawing.Point(75, 49);
+            this.locationText.Name = "locationText";
+            this.locationText.Size = new System.Drawing.Size(406, 20);
+            this.locationText.TabIndex = 1;
+            // 
+            // tagText
+            // 
+            this.tagText.Location = new System.Drawing.Point(75, 141);
+            this.tagText.Name = "tagText";
+            this.tagText.Size = new System.Drawing.Size(406, 20);
+            this.tagText.TabIndex = 2;
+            // 
+            // quoteText
+            // 
+            this.quoteText.Location = new System.Drawing.Point(75, 79);
+            this.quoteText.Name = "quoteText";
+            this.quoteText.Size = new System.Drawing.Size(406, 56);
+            this.quoteText.TabIndex = 3;
+            this.quoteText.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Person:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Location:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Quote:";
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(406, 190);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 7;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(325, 189);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 8;
+            this.okButton.Text = "Accept";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Tags:";
+            // 
+            // EditQuote
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 226);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.quoteText);
+            this.Controls.Add(this.tagText);
+            this.Controls.Add(this.locationText);
+            this.Controls.Add(this.authorText);
+            this.Name = "EditQuote";
+            this.Text = "Edit Quote";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox authorText;
+        private System.Windows.Forms.TextBox locationText;
+        private System.Windows.Forms.TextBox tagText;
+        private System.Windows.Forms.RichTextBox quoteText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ QuotesDB/EditQuote.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QuotesDB
+{
+    public partial class EditQuote : Form
+    {
+        private string origQuote;
+        private string origAuthor;
+        private string origLoc;
+        private string[] origTags;
+
+        public EditQuote(string quoteString)
+        {
+            InitializeComponent();
+
+            string[] arg = parseString(quoteString);
+
+            quoteText.Text = arg[0];
+            authorText.Text = arg[1].Trim();
+            locationText.Text = arg[2].Substring(1);
+        }
+
+        /// <summary>
+        /// Parses a string and breaks it into its components.
+        /// </summary>
+        /// <param name="quoteString"></param>
+        /// <returns>String array: quote, author, location</returns>
+        private string[] parseString(string quoteString)
+        {
+            char[] delimiters = { '~' };
+            string[] vals = new string[3];
+
+            string[] tmp = quoteString.Split(delimiters);
+            vals[0] = tmp[0];
+
+            delimiters[0] = ',';
+            tmp = tmp[1].Split(delimiters);
+
+            vals[1] = tmp[0];
+
+            if (tmp.Length == 2)
+            {
+                vals[2] = tmp[1];
+            }
+            else
+            {
+                vals[2] = " ";
+            }
+
+
+            return vals;
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
